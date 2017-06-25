ceb8dedd9dbf2a692163b00f2f2bf8c00761c80b
orancummins <oran.cummins@gmail.com>
2017-06-25 01:27:15 +0100

    Test Update

--- src/test/java/com/oran/RandomNumberConfigurationTests.java
+++ src/test/java/com/oran/RandomNumberConfigurationTests.java
@@ -19,6 +19,7 @@ import static org.assertj.core.api.BDDAssertions.then;
 /**
  * Basic integration tests for service demo application.
  *
+ *
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = RandomNumberConfiguration.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
